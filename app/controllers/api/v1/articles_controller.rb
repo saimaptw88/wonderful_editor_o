e9fcc4417e477f6fc1ffa00c9704c134aa7d04a1
斎藤正大 <saima.ptw88@gmail.com>
2020-10-01 20:32:51 +0900

    showメソッドを追加

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -11,6 +11,25 @@ module Api
         @article = Article.find(params[:id])
         # render json: @article
       end
+
+      def create
+        article = Article.new(article_params)
+        article.save!
+
+        render :show
+      end
+
+      def update
+      end
+
+      def destroy
+      end
+
+      private
+
+        def article_params
+          params.request(:article).permit(:title, :body)
+        end
     end
   end
 end
