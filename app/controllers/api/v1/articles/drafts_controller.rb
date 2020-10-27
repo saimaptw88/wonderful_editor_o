c827f237b5e868c8caab03734d8215546d03fd5d
斎藤正大 <saima.ptw88@gmail.com>
2020-10-27 12:46:46 +0900

    showメソッドにselializerを追加

--- app/controllers/api/v1/articles/drafts_controller.rb
+++ app/controllers/api/v1/articles/drafts_controller.rb
@@ -1,15 +1,15 @@
-class Api::V1::Articles::DraftsController < ApplicationController
+class Api::V1::Articles::DraftsController < Api::V1::BaseApiController
   # Userがサインインしていなければ401エラーを返す
   before_action :authenticate_api_v1_user!
 
   def index
     articles = current_api_v1_user.articles.draft.order(updated_at: :desc)
-    render json: articles
+    render json: articles, each_serializer: Api::V1::Articles::DraftsSerializer
   end
 
   def show
     article = current_api_v1_user.articles.draft.find(params[:id])
-    render json: article
+    render json: article, serializer: Api::V1::Articles::DraftsSerializer
   end
 
   private
