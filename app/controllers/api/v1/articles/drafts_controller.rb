76572d3a62eac291c6a9c207ca570e3d1fe9988b
斎藤正大 <saima.ptw88@gmail.com>
2020-10-27 21:01:53 +0900

    修正

--- app/controllers/api/v1/articles/drafts_controller.rb
+++ app/controllers/api/v1/articles/drafts_controller.rb
@@ -4,12 +4,12 @@ class Api::V1::Articles::DraftsController < ApplicationController
 
   def index
     articles = current_api_v1_user.articles.draft.order(updated_at: :desc)
-    render json: articles, each_serializer: Api::V1::Articles::DraftSerializer
+    render json: articles, each_serializer: Api::V1::Articles::DraftsSerializer
   end
 
   def show
     article = current_api_v1_user.articles.draft.find(params[:id])
-    render json: article, serializer: Api::V1::Articles::DraftSerializer
+    render json: article, serializer: Api::V1::Articles::DraftsSerializer
   end
 
   private
