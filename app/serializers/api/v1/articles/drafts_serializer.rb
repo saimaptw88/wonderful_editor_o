1c6ec6c56a77bcd8039d0bcb7ecffe2b1f263745
斎藤正大 <saima.ptw88@gmail.com>
2020-10-27 21:06:00 +0900

    修正

--- app/serializers/api/v1/articles/drafts_serializer.rb
+++ app/serializers/api/v1/articles/drafts_serializer.rb
@@ -1,4 +1,4 @@
-class Api::V1::Articles::DraftSerializer < ActiveModel::Serializer
+class Api::V1::Articles::DraftsSerializer < ActiveModel::Serializer
   attributes :id, :title, :status, :updated_at
   belongs_to :user, serializer: Api::V1::UserSerializer
 end
