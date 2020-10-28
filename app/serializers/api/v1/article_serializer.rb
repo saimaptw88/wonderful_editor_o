6f21b0f6ffeb049ff13ea8d0d18890756b3c1665
斎藤正大 <saima.ptw88@gmail.com>
2020-10-28 19:22:17 +0900

    修正

--- app/serializers/api/v1/article_serializer.rb
+++ app/serializers/api/v1/article_serializer.rb
@@ -1,4 +1,4 @@
 class Api::V1::ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :status, :updated_at
+  attributes :id, :title, :status, :updated_at, :body
   belongs_to :user, serializer: Api::V1::UserSerializer
 end
