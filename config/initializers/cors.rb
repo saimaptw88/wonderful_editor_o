40ee22e9cc7ab9cbddcb4b1af7e4ca2fc23b5566
斎藤正大 <saima.ptw88@gmail.com>
2021-02-27 08:46:31 +0900

    修正しました

---
+++ config/initializers/cors.rb
@@ -0,0 +1,13 @@
+# config/initializers/cors.rb
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "https://blog-edit.herokuapp.com/, http://localhost:8080"
+    resource "*",
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head],
+
+             # cookieを利用する場合
+             credentials: true
+  end
+end
